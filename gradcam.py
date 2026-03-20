feb2faedb52640a5f41b27d1ee20a00fc6eb0fc5
lavanya-sathasivam <lavanyasatha66@gmail.com>
2026-03-20 18:49:40 +0530

    GradCAM feature added

---
+++ gradcam.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+def generate_gradcam(model, img_array, last_conv_layer_name="top_conv"):
+    grad_model = tf.keras.models.Model(
+        [model.inputs],
+        [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+    with tf.GradientTape() as tape:
+        conv_outputs, predictions = grad_model(img_array)
+        loss = predictions[:, tf.argmax(predictions[0])]
+
+    grads = tape.gradient(loss, conv_outputs)
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+    conv_outputs = conv_outputs[0]
+    heatmap = conv_outputs @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+    heatmap = tf.maximum(heatmap, 0) / tf.reduce_max(heatmap)
+    return heatmap.numpy()
+
+def overlay_heatmap(original_img, heatmap):
+    heatmap = cv2.resize(heatmap, (224, 224))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    overlay = heatmap * 0.4 + original_img
+    return overlay.astype("uint8")
